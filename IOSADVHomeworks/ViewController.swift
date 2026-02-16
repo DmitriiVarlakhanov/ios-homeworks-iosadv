a7aa70151eb99d9d28084a9aae56d6c710006532
Dmitrii Varlakhanov <dmitriivarlakhanov@gmail.com>
2026-02-16 13:32:07 +0300

    Initial Commit

---
+++ IOSADVHomeworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSADVHomeworks
+//
+//  Created by Dmitrii Varlakhanov on 2/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
